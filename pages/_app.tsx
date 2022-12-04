1dddc925dcd670ef05155395c87900e299557fcd
민경호 <1736s@naver.com>
2022-12-04 12:38:38 +0900

    remove Layout component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,18 @@
 import type { AppProps } from "next/app";
 import "../styles/global.scss";
-import Layout from "../components/Layout/Layout";
+import HeadInfo from "../components/HeadInfo/HeadInfo";
+import Header from "../components/Header/Header";
+import Footer from "../components/Footer/Footer";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <HeadInfo />
+      <Header />
+      <main>
+        <Component {...pageProps} />
+      </main>
+      <Footer />
+    </>
   );
 }
