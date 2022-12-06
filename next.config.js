c163477057617d4ca9d238ea5c404f020f18b94e
민경호 <1736s@naver.com>
2022-12-07 02:25:29 +0900

    add season imgs

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    domains: ["ssl.nexon.com", "fo4.dn.nexoncdn.co.kr"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
