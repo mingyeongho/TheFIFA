4dc9ee2e989dc77e9c6c5768a3e1c059456cbe4e
민경호 <1736s@naver.com>
2023-01-24 20:34:26 +0900

    fix: 뒤로가기 시 bg_img가 안나오는 오류 수정

--- src/components/SearchPage/SearchPage.style.tsx
+++ src/components/SearchPage/SearchPage.style.tsx
@@ -1,10 +1,11 @@
 import styled from "styled-components";
+import { Image } from "../../styles/Variable";
 
 export const SearchPage = styled.main`
   display: flex;
   justify-content: center;
   align-items: center;
-  background-image: url("src/assets/bg_img.jpeg");
+  background-image: url("/src/assets/bg_img.jpeg");
   background-repeat: no-repeat;
   background-size: cover;
 `;
--- src/styles/Variable.tsx
+++ src/styles/Variable.tsx
@@ -1,1 +1,5 @@
 export const Palette = {};
+
+export const Image = {
+  BackgroundImage: "/Users/mingyeongho/Coding/thefifa/src/assets/bg_img.jpeg",
+};
