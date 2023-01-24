6fed1f751fa59e61d1964ad9ee006a196f16c570
민경호 <1736s@naver.com>
2023-01-24 16:30:10 +0900

    feat: 구단주 검색 시 /user/nickname으로 라우팅

--- src/components/Common/Search/Search.tsx
+++ src/components/Common/Search/Search.tsx
@@ -2,12 +2,20 @@ import { FormEvent } from "react";
 import * as S from "./Search.style";
 import { SearchProps } from "../../../utils/interface";
 import getUser from "./apis/getUser";
+import { useNavigate } from "react-router-dom";
 
 const Search = ({ label, name, value, placeholder, onChange }: SearchProps) => {
+  const naviagte = useNavigate();
+
   const onSubmit = async (e: FormEvent) => {
     e.preventDefault();
     const res = await getUser({ nickname: value });
-    console.log(res.data);
+    if (res.data) {
+      const { nickname } = res.data;
+      naviagte(`/user/${nickname}`);
+    } else {
+      alert("존재하지 않은 구단주입니다.");
+    }
   };
 
   return (
--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Index from "../pages/Index";
+import User from "../pages/User";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Index />} />
+        <Route path="/user/:nickname" element={<User />} />
       </Routes>
     </BrowserRouter>
   );
